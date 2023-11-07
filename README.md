b3eec0eb600bb9f1cba1494f09cb802b17b06361
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2023-11-07 19:16:01 +0100

    works mostly ok

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculator
+
+No OOP here, in the end of the project i started to regret it. 
+Known bugs i am familiar with: 
+-when you press different function buttons without entering number, you'll get unpredictable -behaviour R button works same as C
+
+Had to fix this (and visuals), but need to take break from it
--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let displayContent = '';
 let functionButtonClicked = 0;
 let operationType = '';
 let number1 = 0;
-
-
+let number2 = 0;
+let doIclearScreen = 0;
+let doIwaitForInput = 0;
 const numberButton = document.querySelectorAll('.numberButton');
 numberButton.forEach(button => {
     button.addEventListener('click', (e) =>{
-        console.log(e.target.id);
         pushToDisplay(e.target.id);
     })
 })
@@ -15,53 +15,78 @@ numberButton.forEach(button => {
 const functionButton = document.querySelectorAll('.functionButton');
 functionButton.forEach(button => {
     button.addEventListener('click', (e) =>{
-        operationType = e.target.id;
         functionButtonClicked = 1;
-        if(e.target.id === '='||number1!=0) {
-            operate(number1, display, operationType);
+
+        number2 = parseFloat(display.textContent);
+        if(e.target.id==="="||number1!=0) {
+            console.log(""+number1 +operationType+ number2);
+            operate();
+            doIclearScreen = 1;
         }
         else{
         number1 = parseFloat(display.textContent);
+        operationType=e.target.id;
         }
-        console.log("number 1: "+ number1);
-
+        if(e.target.id!="=") operationType = e.target.id;
     })
 })
 
 const specialButton = document.querySelectorAll('.specialButton');
 specialButton.forEach(button => {
     button.addEventListener('click', (e) =>{
-        if(e.target.id==='C') display.textContent='0';
+        if(e.target.id==='C') {
+            display.textContent='0';
+            number1 = 0;
+            number2 = 0;
+            operationType = '';
+        }
+        if(e.target.id==='R') {
+            display.textContent='0';
+            number1 = 0;
+            number2 = 0;
+            operationType = '';
+        }
     })
 })
 
 const display = document.getElementById('display');
 function pushToDisplay(item){
-    if (!functionButtonClicked)
+    if (doIclearScreen === 1) {
+        display.textContent = '0';
+        doIclearScreen = 0;
+    }
+    if (!functionButtonClicked){
     display.textContent = display.textContent ==='0'  ? item : display.textContent + item;
+    }
     else{
     display.textContent = item;
     functionButtonClicked = 0;
     }
 }
 
-function operate(number1, display, operationType){
-    console.log(operationType);
+function operate(){
     switch(operationType){
         case '+':
-            pushToDisplay(number1 + parseFloat(display.textContent));
+            number1 = number1 + parseFloat(number2);
+            pushToDisplay(number1);
             break;
         case '-':
-            pushToDisplay(number1 - parseFloat(display.textContent));
+            number1 = number1 - parseFloat(number2);
+            pushToDisplay(number1);
+            
             break;
         case '*':
-            pushToDisplay(number1 * parseFloat(display.textContent));
+            number1 = number1 * parseFloat(number2);
+            pushToDisplay(number1);
             break;
         case '/':
-            pushToDisplay(number1 / parseFloat(display.textContent));
+            number1 = number1 / parseFloat(number2);
+            pushToDisplay(number1);
             break;
+    
     }
-
+    doIclearScreen = 1;
+    doIwaitForInput = 1;
 }
 
 
